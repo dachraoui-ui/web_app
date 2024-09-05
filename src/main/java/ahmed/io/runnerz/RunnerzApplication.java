11dcbf52af9e5154f25987879937cca0b51a6360
ahmed dacharoui <dachraouia193@gmail.com>
2024-09-05 23:08:51 +0100

    add some features

--- src/main/java/ahmed/io/runnerz/RunnerzApplication.java
+++ src/main/java/ahmed/io/runnerz/RunnerzApplication.java
@@ -8,10 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import javax.tools.JavaFileManager;
-import java.time.LocalDateTime;
 
 
 @SpringBootApplication
