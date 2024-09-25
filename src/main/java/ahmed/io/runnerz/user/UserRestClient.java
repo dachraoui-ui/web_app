6c47f4ccf377e1b22ef133b39c77c5f649e5724a
ahmed dacharoui <dachraouia193@gmail.com>
2024-09-25 22:23:21 +0100

    add userRestClient

---
+++ src/main/java/ahmed/io/runnerz/user/UserRestClient.java
@@ -0,0 +1,24 @@
+package ahmed.io.runnerz.user;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Component
+public class UserRestClient {
+    private final RestClient restClient;
+
+    public UserRestClient(RestClient.Builder builder) {
+        this.restClient = builder
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .build();
+    }
+    public List<User> findAll() {
+        return this.restClient.get()
+                .uri("/users")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<User>>() {});
+    }
+}
