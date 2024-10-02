2c476442ae406308ed0481e57da42adf9e4e838b
dachraoui ahmed <111989744+dachraoui-ui@users.noreply.github.com>
2024-10-02 22:56:09 +0100

    Update RunController.java

--- src/main/java/ahmed/io/runnerz/run/RunController.java
+++ src/main/java/ahmed/io/runnerz/run/RunController.java
@@ -54,4 +54,5 @@ public class RunController {
     List<Run> findByLocation(@PathVariable Location location){
         return runRepository.findRunByLocation(location);
     }
+    // add find by location
 }
